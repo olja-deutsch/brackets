c242e92e83450232f54e71386c38000c86e81806
Olga Murina <olgamurina92@gmail.com>
2020-10-18 15:10:00 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 !== 0) {
+        return false;
+    } else {
+      const combinedConfig = bracketsConfig.map((item) => item.join(""));
+
+      str = str.split('');
+
+      for (let i = 0; i < str.length; i++) {
+        if (combinedConfig.includes(str[i] + str[i + 1])) {
+          str.splice(i, 2);
+          i = -1;
+        }
+      };
+
+      return str.length ? false : true;
+    };
 }
